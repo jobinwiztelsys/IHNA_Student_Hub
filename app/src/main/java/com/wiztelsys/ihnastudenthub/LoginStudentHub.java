3a0574687b1d75212da74fead7c108b0db4b2eb4
jobinwiztelsys <jobin@wiztelsys.com>
2015-07-10 13:00:32 +0530

    pin login activity completed

--- app/src/main/java/com/wiztelsys/ihnastudenthub/LoginStudentHub.java
+++ app/src/main/java/com/wiztelsys/ihnastudenthub/LoginStudentHub.java
@@ -11,7 +11,7 @@ public class LoginStudentHub extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_student_hub);
+        setContentView(R.layout.pin_login_activity);
 // test command
         // updated by rajeev
 
